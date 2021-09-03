fc1e3202a1f26f78805dd426b2296f318df8eb53
sam-captain <kamangacaptainirungu0625@gmail.com>
2021-09-03 11:27:21 +0300

    added models and blockcss

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,37 @@
+import contextlib
+from typing import ContextManager
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Create your views here.    
+def home(request):
+    Context = {
+        'heading' : 'Welcome to my Landing page',
+         'services' : ["coding", "eating", "sleeping"],
+         'posts' : ["Big blue world","Out of space", "A-O-A-Okay"],
+    }
+    return render(request,"home.html", Context)
+
+def contact(request):
+    Context = {
+        'number' : '2547256995129',
+        'email' :'Samkent@gmail.com',
+        'address': 'juja kiambu 02-40054',
+        
+    }
+    return render(request,"contact.html", Context)
+
+def blog(request):
+    context = {
+        'header' : 'Welcome to my blog posts',
+        'posts' : ["Big blue world","Out of space", "A-O-A-Okay", "Seven greate planets","Nice water breeze","bubble honey bee"],
+    }
+    return render(request,"blog/blog.html", context)
+
+def myposts(request):
+    context = {
+        'details' : ["Run a contest","Review books/products/films","Interview someone",],
+
+    }
+    return render(request,"post-details.html",context)
+
--- django_blog/urls.py
+++ django_blog/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home),
+    path('contact', views.contact, name="contact"),
+    path('blog', views.blog),
+    path('myposts',views.myposts)
 ]
